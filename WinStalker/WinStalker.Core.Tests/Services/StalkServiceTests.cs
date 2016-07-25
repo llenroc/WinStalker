07bc4b3e178717376db4a1d6bac2bd1fdea6b743
marcioggs <marcioggs@gmail.com>
2016-07-25 18:39:43 -0300

    Adicionados TODO's de teste.

--- WinStalker/WinStalker.Core.Tests/Services/StalkServiceTests.cs
+++ WinStalker/WinStalker.Core.Tests/Services/StalkServiceTests.cs
@@ -9,15 +9,24 @@ namespace WinStalker.Core.Tests
 
         public StalkServiceTests()
         {
-            //TODO: Mockar API e injetar dependência.
+            //TODO: Refatorar StalkService para desacoplar API e injetar a dependência mockada por aqui.
             _stalkService = new StalkService();
         }
 
         [Fact]
         public void TesteTest()
         {
+            //TODO: Exemplo de teste, remover após criar os testes reais.
             Assert.Equal("Márcio Gabriel", _stalkService.GetPerson("marcioggs@gmail.com").FullName);
         }
+
+        /* TODO: Escrever teste para:
+            - Obter pessoa com sucesso
+            - Obter ícone da rede social
+            - Pessoa não encontrada
+            - Dados da pessoa sendo preparados
+            - Erro genérico da API
+        */
 
     }
 }
