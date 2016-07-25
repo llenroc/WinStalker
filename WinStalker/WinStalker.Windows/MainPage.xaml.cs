d483113da3903c400fb34e01d3e0c781dc096d3b
marcioggs <marcioggs@gmail.com>
2016-07-25 19:05:06 -0300

    Incluída informação de nome da rede social.

--- WinStalker/WinStalker.Windows/MainPage.xaml.cs
+++ WinStalker/WinStalker.Windows/MainPage.xaml.cs
@@ -3,18 +3,13 @@ using Windows.UI.Xaml.Controls;
 using WinStalker.Core.Model;
 using WinStalker.Core.Services;
 
-
-// O modelo do item de página em branco está documentado em http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
-
 namespace WinStalker.Windows
 {
-    /// <summary>
-    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
         private IStalkService _stalkService;
         private Person _person;
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -23,15 +18,19 @@ namespace WinStalker.Windows
 
         private void ButtonStalk_OnClick(object sender, RoutedEventArgs e)
         {
+            _person  = _stalkService.GetPerson(TextBoxEmail.Text);
 
-            _person  = _stalkService.GetPerson(TextBoxEmail.Text );
-            TextBlockReturn.Text = "Nome:"+_person.FullName;
-            TextBlockReturn.Text += "\nGênero:"+_person.Gender;
+            //TODO: Mapear os campos separadamente no XAML.
+            TextBlockReturn.Text = "Nome: "+_person.FullName;
+            TextBlockReturn.Text += "\nGênero: "+_person.Gender;
             TextBlockReturn.Text += "\n\n Social Networks:\n";
-            foreach ( SocialNetwork sn in _person.SocialNetworks)
+
+            foreach (SocialNetwork sn in _person.SocialNetworks)
             {
+                TextBlockReturn.Text += "\n Name: " + sn.TypeName;
                 TextBlockReturn.Text += "\n URL: " + sn.Url;
                 TextBlockReturn.Text += "\n UserName: " + sn.Username;
+                TextBlockReturn.Text += "\n";
             }
             
         }
